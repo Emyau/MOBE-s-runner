22a9ed10efd1daee805c8e516c4681a15d52ccf5
pixmacfy <remy.peyrich@gmail.com>
2023-01-26 12:09:16 +0100

    Added title screen activity and basic graphical stuff

---
+++ app/src/main/java/com/ut3/moberunner/TitleScreenActivity.java
@@ -0,0 +1,14 @@
+package com.ut3.moberunner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TitleScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title_screen);
+    }
+}
