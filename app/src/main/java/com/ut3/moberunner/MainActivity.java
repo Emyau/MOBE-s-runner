60a221daf6b5dc8550227296c22a7ba57a4e9676
Emilie Teytau <emilie.teytau@orange.fr>
2023-02-09 10:56:50 +0100

    get rid of window parameter in java code

--- app/src/main/java/com/ut3/moberunner/MainActivity.java
+++ app/src/main/java/com/ut3/moberunner/MainActivity.java
@@ -12,7 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(new ChickenView(this));
     }
 }
