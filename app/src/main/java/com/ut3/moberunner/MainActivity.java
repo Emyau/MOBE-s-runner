a4058b8a52b6ba18b9c7a0e0b8721b0500b6802b
Emyau <56170314+Emyau@users.noreply.github.com>
2023-02-09 11:08:24 +0100

    Scrolling background (#2)
    * wip
    
    * wip
    
    * get rid of window parameter in java code
    
    * clean code

--- app/src/main/java/com/ut3/moberunner/MainActivity.java
+++ app/src/main/java/com/ut3/moberunner/MainActivity.java
@@ -3,12 +3,15 @@ package com.ut3.moberunner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(new ChickenView(this));
     }
 }
